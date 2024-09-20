d3b66ce0bad21d7f0331756fcfcb4fe396ee417c
test <arjithar8@gmail.com>
2024-09-21 00:41:05 +0530

    Added multiple options to pop up menu; works fine

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 import GObject from 'gi://GObject';
 import St from 'gi://St';
+import GLib from 'gi://GLib';
 
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
@@ -13,6 +14,7 @@ let globalSettings;
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
     _init() {
+
         super._init(0.0, _('Focus Extension'));
         const work_path = "dialog-information"
         const pro_path = "edit-undo"
@@ -42,10 +44,7 @@ class Indicator extends PanelMenu.Button {
         } else {
             current_icon = break_icon
         }
-        
-
-        // print()
-        
+                
         this.add_child(current_icon)
         // this.add_child(new St.Icon({
         //     icon_name: 'face-smile-symbolic',
@@ -54,13 +53,28 @@ class Indicator extends PanelMenu.Button {
         // }));
         
 
-        let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
-        item.connect('activate', () => {
-            console.log(globalSettings.get_string('current-status'))
-            console.log(globalSettings.get_value('show-indicator').deepUnpack())
-            Main.notify(_('Whatʼs up, folks?'), "How are you");
+        // let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
+        // item.connect('activate', () => {
+        //     console.log(globalSettings.get_string('current-status'))
+        //     console.log(globalSettings.get_value('show-indicator').deepUnpack())
+        //     Main.notify(_('Whatʼs up, folks?'), "How are you");
+        // });
+        // this.menu.addMenuItem(item);
+
+
+        let item1 = new PopupMenu.PopupMenuItem(_('Work'));
+        let item2 = new PopupMenu.PopupMenuItem(_('Procrastinate'));
+        let item3 = new PopupMenu.PopupMenuItem(_('Break'));
+
+        item1.connect('activate', () => {
+            console.log("Hellooooo");
+            // Here aliases wont work( aliases as in created using bashrc but you can have .sh files)
+            GLib.spawn_command_line_async("notify-send 'Hello, World!' 'This is a simple notification.'");
         });
-        this.menu.addMenuItem(item);
+        this.menu.addMenuItem(item1);
+        this.menu.addMenuItem(item2)
+        this.menu.addMenuItem(item3)
+
     }
 });
 
